95d52edcf6a2be8875216aa78c7e3c422938dcef
Zurab <mcgeni2003@mail.ru>
2023-11-15 18:28:08 +0300

    updated TODO

--- state-dom.js
+++ state-dom.js
@@ -3,32 +3,61 @@ const arr = [
         text: 'first ',
         done: false
     },
-    {
-        text: 'second ',
-        done: false
-    },
-    {
-        text: 'third ',
-        done: true
-    },
-    {
-        text: 'fourth ',
-        done: false
-    },
-    {
-        text: 'fifth ',
-        done: true
-    },
+
 ]
 
+const checkTodo = function(i) {
+    arr[i].done = !arr[i].done
+}
+const rem = function (i) {
+    arr.splice(i, 1)
+    render(arr)
+}
+const addTodo = function (txt) {
+    arr.push({ text: txt, done: false })
+    return render(arr)
+}
+
+const list = document.getElementById('list')
+const input = document.getElementById('input')
+input.style.fontFamily = 'cursive'
+
+
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    addTodo(input.value)
+    input.value = ''
+
+})
+
 const render = (tasks) => {
-    const list = document.getElementById('list')
+    list.innerHTML = ''
+    tasks.forEach((i, index) => {
+        const checkBox = document.createElement('input')
+        checkBox.type = 'checkBox'
+        checkBox.checked = i.done
 
-
-    tasks.forEach(i => {
+        checkBox.addEventListener('click', () => {
+            checkTodo(index)
+            
+        })
+        const delButton = document.createElement('button')
+        delButton.textContent = 'x'
         let textList = document.createElement('p')
         textList.textContent = i.text
+        textList.style.fontFamily = 'cursive'
         list.append(textList)
+        textList.append(delButton)
+        textList.prepend(checkBox)
+
+        delButton.addEventListener('click', () => {
+        rem(index)
+        })
     })
+    
+   
 }
+
+
 console.log(render(arr));
